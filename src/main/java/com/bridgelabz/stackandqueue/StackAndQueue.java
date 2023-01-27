b83f8f7daa89f8cf1a42c87cf118eceed17a6aa9
AryaBhuwan-98 <aryabhuwan873@gmail.com>
2023-01-26 22:03:54 -0800

    Added:Ability to create a Stack Linked List Operation

---
+++ src/main/java/com/bridgelabz/stackandqueue/StackAndQueue.java
@@ -0,0 +1,43 @@
+package com.bridgelabz.stackandqueue;
+
+public class StackAndQueue {
+    
+    Node top;
+	Node next;
+
+	public class Node {
+		int data;
+		Node next;
+	}
+
+	public void Node() {
+		this.top = null;
+	}
+
+	public void push(int number) {
+		Node node = new Node();
+		node.data = number;
+		node.next = top;
+		top = node;
+	}
+
+	public void printStack() {
+		Node node = top;
+		while (node != null) {
+			System.out.println(node.data);
+			node = node.next;
+
+		}
+	}
+
+	public static void main(String[] args) {
+		StackAndQueue stackoperation = new StackAndQueue();
+		stackoperation.push(70);
+		stackoperation.push(30);
+		stackoperation.push(56);
+		stackoperation.printStack();
+
+	}
+
+}
+
