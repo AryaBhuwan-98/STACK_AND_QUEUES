effb480cd4085975770460b1c5e2f9d77d282126
AryaBhuwan-98 <aryabhuwan873@gmail.com>
2023-01-26 21:58:00 -0800

    Add:Welcome to the Stack And Queue Data Structure

---
+++ README.md
@@ -0,0 +1,1 @@
+# STACK_AND_QUEUES
---
+++ src/main/java/com/bridgelabz/stackandqueue/App.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.stackandqueue;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to the STACK AND QUEUE DATA STRUCTURE" );
+    }
+}
